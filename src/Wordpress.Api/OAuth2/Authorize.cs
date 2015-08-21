f2c60312734942e694d132f8b3db9281b8d4aa9e
michaelsync <mchlsync@gmail.com>
2015-08-21 13:58:33 +1000

    added api and test project

---
+++ src/Wordpress.Api/OAuth2/Authorize.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordpress.Api.OAuth2 {
+    public class Authorize {
+    }
+}
